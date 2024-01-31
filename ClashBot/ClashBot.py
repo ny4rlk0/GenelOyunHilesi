fee9f905d64f1731d57d2ba83eeaae8a528250f6
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2024-01-31 19:29:28 +0300

    Update ClashBot.py

--- ClashBot/ClashBot.py
+++ ClashBot/ClashBot.py
@@ -1,19 +1,19 @@
 # This Python file uses the following encoding: utf-8
 from ctypes import Array
 import os
-#os.execute("pip install pyautogui opencv-python Pillow, colorama")
+#os.execute("pip install pyautogui opencv-python Pillow colorama")
 from pyautogui import *
 import numpy as np
 import pyautogui,time,random, cv2, datetime
 from PIL import ImageGrab
 from colorama import Fore, init
 
-#İşlem geciktirme
-tiklama_gecikmesi=0.1 ### Tıklamalar arasında geçen süre (Saniye cinsinden)
-islem_gecikmesi=1 ### Yapılan işlemler arasında geçen süre (Saniye cinsinden)
+#Ä°ÅŸlem geciktirme
+tiklama_gecikmesi=0.1 ### TÄ±klamalar arasÄ±nda geÃ§en sÃ¼re (Saniye cinsinden)
+islem_gecikmesi=1 ### YapÄ±lan iÅŸlemler arasÄ±nda geÃ§en sÃ¼re (Saniye cinsinden)
 herseyi_tekrarlama_gecikmesi= 30 #7200 #2 saat
 
-#Dosya yolları
+#Dosya yollarÄ±
 cwd = os.getcwd()+"\\";
 
 iksir_resim_yolu = cwd+"resources\\elixir_popup.png";
@@ -36,7 +36,7 @@ sifirla_resim_benzerlik_yuzdesi= .59
 carpi_resim_benzerlik_yuzdesi=.60
 
 #Deneysel
-hata_ayikla=False; #True tıklanan alanları kırmızı dikdörtgen içine alarak göster.
+hata_ayikla=False; #True tÄ±klanan alanlarÄ± kÄ±rmÄ±zÄ± dikdÃ¶rtgen iÃ§ine alarak gÃ¶ster.
 tiklama_kaydi=True;
 cift_tiklama=False;
 
@@ -47,7 +47,7 @@ def ekrana_tikla(x_pos,y_pos):
     pyautogui.mouseDown(button='left',x=x_pos,y=y_pos)
     bekle(tiklama_gecikmesi)
     pyautogui.mouseUp(button='left',x=x_pos,y=y_pos)
-    #Çift tıklama seçiliyse ikinci defa tıklayalım
+    #Ã‡ift tÄ±klama seÃ§iliyse ikinci defa tÄ±klayalÄ±m
     if(cift_tiklama):
         bekle(tiklama_gecikmesi)
         pyautogui.mouseDown(button='left',x=x_pos,y=y_pos)
@@ -57,7 +57,7 @@ def ekrana_tikla(x_pos,y_pos):
     
 def ekran_tarama():#Ekran resmini kaydetme
     ekran= ImageGrab.grab()
-    ekran=ekran.convert('RGBA');#32 bit renk derinliği, bu kod silinirse 24 bit olacak ekran ve hata verecek
+    ekran=ekran.convert('RGBA');#32 bit renk derinliÄŸi, bu kod silinirse 24 bit olacak ekran ve hata verecek
     ekran.save("ekran_tarama.png","PNG")
     
 def hedef_bul(aranacak_resim,benzerlik_yuzdesi,tikla=False,islem=""):
@@ -66,26 +66,26 @@ def hedef_bul(aranacak_resim,benzerlik_yuzdesi,tikla=False,islem=""):
     harita_0= cv2.imread(ekran_tarama_sonuc_yolu, cv2.IMREAD_UNCHANGED)
     hedef_0= cv2.imread(aranacak_resim, cv2.IMREAD_UNCHANGED)
     tarama_sonucu= cv2.matchTemplate(harita_0, hedef_0, cv2_tarama_yontemi)
-    #Buradaki min max değer, aradığınla en çok eşleşen ve en az eşleşen demek
+    #Buradaki min max deÄŸer, aradÄ±ÄŸÄ±nla en Ã§ok eÅŸleÅŸen ve en az eÅŸleÅŸen demek
     min_deg, maks_deg, min_degerin_konumu, maks_degerin_konumu = cv2.minMaxLoc(tarama_sonucu);
     genislik=hedef_0.shape[1]
     yukseklik=hedef_0.shape[0]
     #cv2.rectangle(harita_0, maks_degerin_konumu,(maks_degerin_konumu[0]+genislik,maks_degerin_konumu[1]+yukseklik), (0,0,255), 2 ) #B, G, R
     #cv2.imshow("Hedef Bulundu", tarama_sonucu)
     y_konumu, x_konumu = np.where(tarama_sonucu>=benzerlik_yuzdesi)
-    #Ekranda aranan resim bulunamadı.
+    #Ekranda aranan resim bulunamadÄ±.
     if(len(x_konumu)<=0): 
         return
-    #Benzer konumları birlestirelim
+    #Benzer konumlarÄ± birlestirelim
     konumlandirma_dikdortgenleri=[]
-    #Burada iki kere eklememizin sebebi dikdörtgenleri birleştirirken tek bir tane varsa hata vermesi
-    #Bu kod her dikdörtgeni iki kere kaydederek hatanın önüne geçiyor
+    #Burada iki kere eklememizin sebebi dikdÃ¶rtgenleri birleÅŸtirirken tek bir tane varsa hata vermesi
+    #Bu kod her dikdÃ¶rtgeni iki kere kaydederek hatanÄ±n Ã¶nÃ¼ne geÃ§iyor
     for(x,y) in zip(x_konumu, y_konumu):
         konumlandirma_dikdortgenleri.append([int(x),int(y),int(genislik), int(yukseklik)])
         konumlandirma_dikdortgenleri.append([int(x),int(y),int(genislik), int(yukseklik)])
-    #Aynı resmi birden fazla defa işaretlediyse yakın işaretleri birleştirelim
+    #AynÄ± resmi birden fazla defa iÅŸaretlediyse yakÄ±n iÅŸaretleri birleÅŸtirelim
     konumlandirma_dikdortgenleri, yakinlik= cv2.groupRectangles(konumlandirma_dikdortgenleri, 1, 0.2)
-    #Dikdörtgen çizelim
+    #DikdÃ¶rtgen Ã§izelim
     for (x,y,genislik,yukseklik) in konumlandirma_dikdortgenleri:
         cv2.rectangle(harita_0,(x,y),(x+genislik,y+yukseklik),(0,0,255),2)
         #print("x:"+str(x)+" y:"+str(y)+" genislik:"+str(genislik)+" yukseklik:"+str(yukseklik))
@@ -95,35 +95,35 @@ def hedef_bul(aranacak_resim,benzerlik_yuzdesi,tikla=False,islem=""):
             tik_sayisi=tik_sayisi+1
             print(Fore.LIGHTGREEN_EX+"__Ekran Tiklamasi "+str(tik_sayisi)+"__"+Fore.WHITE)
             print("x:"+str(x)+" y:"+str(y)+" Genislik:"+str(gen)+" Yukseklik:"+str(yuk))
-        #Dosya kaydı (Program her tıkladığında ekran alıntısı alıp tıkladığı yeri işaretlesin)
+        #Dosya kaydÄ± (Program her tÄ±kladÄ±ÄŸÄ±nda ekran alÄ±ntÄ±sÄ± alÄ±p tÄ±kladÄ±ÄŸÄ± yeri iÅŸaretlesin)
         if(tiklama_kaydi):
             mevcut_tarih=str(datetime.datetime.now())
             mevcut_tarih=mevcut_tarih.replace("-","_")
             mevcut_tarih=mevcut_tarih.replace(":","_")
             mevcut_tarih=mevcut_tarih.replace(".","_")
             cv2.imwrite(kayit_yolu+str(mevcut_tarih)+".png",harita_0)
-        #Ekrandaki resimlere tıklayalım
+        #Ekrandaki resimlere tÄ±klayalÄ±m
         for (x,y,genislik,yukseklik) in konumlandirma_dikdortgenleri:
             ekrana_tikla(x,y)
-            #yaptığımız işlemin adını ekrana yazdıralım
+            #yaptÄ±ÄŸÄ±mÄ±z iÅŸlemin adÄ±nÄ± ekrana yazdÄ±ralÄ±m
             print(islem)
-            #eğer iksir, altin topluyorsak ya da tamam tuşuna basıyorsak, yanlış birşeye basmamız ihtimaline karşı
-            #varsa çarpı tuşuna basalım aynı şekilde yine köy ekranındaysak tıklamayı boş bir alana tıklayarak sıfırlayalım
+            #eÄŸer iksir, altin topluyorsak ya da tamam tuÅŸuna basÄ±yorsak, yanlÄ±ÅŸ birÅŸeye basmamÄ±z ihtimaline karÅŸÄ±
+            #varsa Ã§arpÄ± tuÅŸuna basalÄ±m aynÄ± ÅŸekilde yine kÃ¶y ekranÄ±ndaysak tÄ±klamayÄ± boÅŸ bir alana tÄ±klayarak sÄ±fÄ±rlayalÄ±m
             if(" iksir" in islem or " altin" in islem or " tamam" in islem):
                 hedef_bul(carpi_resim_yolu,carpi_resim_benzerlik_yuzdesi,True,str(datetime.datetime.now())+Fore.RED+" carpi tusuna basildi"+Fore.WHITE)
                 hedef_bul(sifirla_resim_yolu,sifirla_resim_benzerlik_yuzdesi,True,str(datetime.datetime.now())+Fore.LIGHTBLUE_EX+" tiklama sifirlandi"+Fore.WHITE)
                 break
-    #Birleştirmeden sonraki konum sayısını ekrana yazdıralım
+    #BirleÅŸtirmeden sonraki konum sayÄ±sÄ±nÄ± ekrana yazdÄ±ralÄ±m
     if (len(konumlandirma_dikdortgenleri)!=0 and len(konumlandirma_dikdortgenleri)!=1):
         print("\nYakin konumlari birlestirmeden sonraki sayi:"+str(len(konumlandirma_dikdortgenleri)))
-    #İşaretlenen yerleri ekranda gösterelim
+    #Ä°ÅŸaretlenen yerleri ekranda gÃ¶sterelim
     if(hata_ayikla):
         cv2.imshow("harita_0",harita_0)
         cv2.waitKey()
         cv2.destroyAllWindows()
 
 init()
-#resources ve kayit klasörleri yoksa oluşturalım
+#resources ve kayit klasÃ¶rleri yoksa oluÅŸturalÄ±m
 if(os.path.exists(cwd+"resources") is not True):
     os.makedirs(cwd+"resources")
 if(os.path.exists(cwd+"kayit") is not True):
