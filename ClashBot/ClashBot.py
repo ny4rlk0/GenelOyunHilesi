2e33d53cefc782105168e6bb203041bb65d49ce9
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2024-02-02 12:35:16 +0300

    Update ClashBot.py

--- ClashBot/ClashBot.py
+++ ClashBot/ClashBot.py
@@ -10,8 +10,8 @@ from colorama import Fore, init
 
 #İşlem geciktirme
 tiklama_gecikmesi=0.1 ### Tıklamalar arasında geçen süre (Saniye cinsinden)
-islem_gecikmesi=0.1 ### Yapılan işlemler arasında geçen süre (Saniye cinsinden)
-herseyi_tekrarlama_gecikmesi= 5 #7200 #2 saat
+islem_gecikmesi=30 ### Yapılan işlemler arasında geçen süre (Saniye cinsinden)
+herseyi_tekrarlama_gecikmesi= 7200 #7200 #2 saat
 
 #Dosya yolları
 cwd = os.getcwd()+"\\";
@@ -32,7 +32,7 @@ iksir_resim_benzerlik_yuzdesi=.61 #
 altin_resim_benzerlik_yuzdesi=.61 #
 oyunu_tekrar_yukle_resim_benzerlik_yuzdesi = .50
 tamam_resim_benzerlik_yuzdesi= .65
-sifirla_resim_benzerlik_yuzdesi= .59
+sifirla_resim_benzerlik_yuzdesi= .60
 carpi_resim_benzerlik_yuzdesi=.60
 
 #Deneysel
@@ -43,6 +43,7 @@ cift_tiklama=False;
 #Değiştirmeye gerek olmayan değişkenler
 fare_konumu_x=10;
 fare_konumu_y=10;
+pyautogui.FAILSAFE=False;
 
 def tarih_saat():
     s=datetime.datetime.now();
@@ -59,7 +60,7 @@ def fare_konumu(islem=""):
     if islem=="kaydet":
        fare_konumu_x, fare_konumu_y= pyautogui.position()
     elif islem=="geri_yukle":
-        pyautogui.moveTo(fare_konumu_x,fare_konumu_y);
+        pyautogui.moveTo(fare_konumu_x,fare_konumu_y)
 
 def bekle(sure):
     time.sleep(sure)
@@ -82,8 +83,8 @@ def ekran_tarama():#Ekran resmini kaydetme
     ekran.save("ekran_tarama.png","PNG")
     
 def hedef_bul(aranacak_resim,benzerlik_yuzdesi,tikla=False,islem=""):
-    bekle(islem_gecikmesi)
-    #ekran_tarama()
+    bekle(tiklama_gecikmesi)
+    ekran_tarama()
     harita_0= cv2.imread(ekran_tarama_sonuc_yolu, cv2.IMREAD_UNCHANGED)
     hedef_0= cv2.imread(aranacak_resim, cv2.IMREAD_UNCHANGED)
     tarama_sonucu= cv2.matchTemplate(harita_0, hedef_0, cv2_tarama_yontemi)
@@ -132,13 +133,15 @@ def hedef_bul(aranacak_resim,benzerlik_yuzdesi,tikla=False,islem=""):
             print(islem);
             ekrana_tikla(x,y)
             fare_konumu("geri_yukle");
+            #bekle(islem_gecikmesi)
             #yaptığımız işlemin adını ekrana yazdıralım
             #print(islem)
             #eğer iksir, altin topluyorsak ya da tamam tuşuna basıyorsak, yanlış birşeye basmamız ihtimaline karşı
             #varsa çarpı tuşuna basalım aynı şekilde yine köy ekranındaysak tıklamayı boş bir alana tıklayarak sıfırlayalım
-            if(" iksir" in islem or " altin" in islem or " tamam" in islem):
+            if(" iksir" in islem or " altin" in islem or " tamam" in islem or " tiklama sifirlandi."):
                 #hedef_bul(carpi_resim_yolu,carpi_resim_benzerlik_yuzdesi,True,str(datetime.datetime.now())+Fore.RED+" carpi tusuna basildi"+Fore.WHITE)
-                hedef_bul(sifirla_resim_yolu,sifirla_resim_benzerlik_yuzdesi,True,str(tarih_saat())+Fore.LIGHTBLUE_EX+" tiklama sifirlandi."+Fore.WHITE)
+                if " tiklama sifirlandi." not in islem:
+                    hedef_bul(sifirla_resim_yolu,sifirla_resim_benzerlik_yuzdesi,True,str(tarih_saat())+Fore.LIGHTBLUE_EX+" tiklama sifirlandi."+Fore.WHITE)
                 break
     #Birleştirmeden sonraki konum sayısını ekrana yazdıralım
     #if (len(konumlandirma_dikdortgenleri)!=0 and len(konumlandirma_dikdortgenleri)!=1):
@@ -157,12 +160,12 @@ if(os.path.exists(cwd+"kayit") is not True):
     os.makedirs(cwd+"kayit")
     
 while (1):
-    for x in range(2):
-        ekran_tarama();
+    for x in range(5):
+        #ekran_tarama();
+        hedef_bul(oyunu_tekrar_yukle_resim_yolu,oyunu_tekrar_yukle_resim_benzerlik_yuzdesi,True,str(tarih_saat())+Fore.BLUE+" oyuna yeniden baglaniliyor."+Fore.WHITE)
         hedef_bul(iksir_resim_yolu,iksir_resim_benzerlik_yuzdesi,True,str(tarih_saat())+Fore.MAGENTA+" iksir"+Fore.WHITE+" toplandi.")
         hedef_bul(carpi_resim_yolu,carpi_resim_benzerlik_yuzdesi,True,str(tarih_saat())+Fore.RED+" carpi tusuna basildi."+Fore.WHITE)
         hedef_bul(altin_resim_yolu,altin_resim_benzerlik_yuzdesi,True,str(tarih_saat())+Fore.YELLOW+" altin"+Fore.WHITE+" toplandi.")
-        hedef_bul(oyunu_tekrar_yukle_resim_yolu,oyunu_tekrar_yukle_resim_benzerlik_yuzdesi,True,str(tarih_saat())+Fore.BLUE+" oyuna yeniden baglaniliyor."+Fore.WHITE)
         hedef_bul(tamam_resim_yolu,tamam_resim_benzerlik_yuzdesi,True,str(tarih_saat())+Fore.GREEN+" Tamam tusuna basildi."+Fore.WHITE)
         #hedef_bul(sifirla_resim_yolu,sifirla_resim_benzerlik_yuzdesi,True,str(datetime.datetime.now())+Fore.LIGHTBLUE_EX+" tiklama sifirlandi"+Fore.WHITE)
         bekle(islem_gecikmesi);
