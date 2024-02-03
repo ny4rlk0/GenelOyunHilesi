c3f8264b6e2d4d6a13070c349ce6fd6c48f885f2
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2024-02-03 17:20:05 +0300

    Add files via upload

---
+++ GenelHileBotu/eklentiler/clashofclans.py
@@ -0,0 +1,25 @@
+# This Python file uses the following encoding: utf-8
+iksir_resim_yolu = "resimler\\clashofclans\\elixir_popup.png";
+altin_resim_yolu = "resimler\\clashofclans\\gold_popup.png";
+tas_resim_yolu = "resimler\\clashofclans\\stone_popup.png";
+oyunu_tekrar_yukle_resim_yolu = "resimler\\clashofclans\\oyunu_tekrar_yukle.png";
+tamam_resim_yolu = "resimler\\clashofclans\\tamam.png";
+sifirla_resim_yolu = "resimler\\clashofclans\\sifirla.png";
+carpi_resim_yolu = "resimler\\clashofclans\\carpi.png";
+yukselt_resim_yolu = "resimler\\clashofclans\\yukselt.png";
+kayit_yolu= "kayit\\clashofclans\\";
+
+#Kalibrasyon
+iksir_resim_benzerlik_yuzdesi=.61 #
+altin_resim_benzerlik_yuzdesi=.61 #
+tas_resim_benzerlik_yuzdesi=.61 #
+oyunu_tekrar_yukle_resim_benzerlik_yuzdesi = .50
+tamam_resim_benzerlik_yuzdesi= .65
+sifirla_resim_benzerlik_yuzdesi= .60
+carpi_resim_benzerlik_yuzdesi=.60
+yukselt_resim_benzerlik_yuzdesi=.60
+
+#Ýþlem geciktirme
+tiklama_gecikmesi=0.1 ### Týklamalar arasýnda geçen süre (Saniye cinsinden)
+islem_gecikmesi=32 ### Yapýlan iþlemler arasýnda geçen süre (Saniye cinsinden)
+herseyi_tekrarlama_gecikmesi= 4200 #7200 #2 saat
